2c4d39c5f689fd9960131260c73a1dcf0b929e55
nummyn0rih <nummyn0rih@gmail.com>
2020-02-11 19:57:17 +0300

    added componet Card

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ import './index.css';
 import MyForm from './Components/MyForm';
 import Component from './Components/Component';
 import TodoBox from './Components/TodoBox';
+import Card from './Components/Card';
+import Modal from './Components/Modal';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Modal />, document.querySelector('.modal'));
+
+const card = (
+  <Card>
+    <Card.Body>
+      <Card.Title>Title 3</Card.Title>
+      <Card.Text>Text 3</Card.Text>
+    </Card.Body>
+  </Card>
+);
+ReactDOM.render(card, document.querySelector('.card'));
+
 ReactDOM.render(<MyForm />, document.querySelector('.form'));
 ReactDOM.render(<Component />, document.querySelector('.component'));
 ReactDOM.render(<TodoBox />, document.querySelector('.todo'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
